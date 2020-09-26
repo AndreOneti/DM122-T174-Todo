97a87c33088d112184c3143a1d6215bd1238cc08
AndreOneti <andreluiz.onete@hotmail.com>
2020-09-26 16:11:29 -0300

    Implemetne save toggle task

--- assets/js/htmlService.js
+++ assets/js/htmlService.js
@@ -1,3 +1,4 @@
+const DONE = "done";
 export default class HtmlService {
 
   constructor(todoService) {
@@ -25,6 +26,19 @@ export default class HtmlService {
     tasks.forEach(task => this.addToHtmlList(task));
   }
 
+  async saveTask(taskId, isDone) {
+    const task = await this.todoService.get(taskId);
+    task.done = isDone;
+    this.todoService.save(task);
+  }
+
+  toggleTask(li) {
+    const taskId = +li.getAttribute("data-item-id");
+    li.classList.toggle(DONE);
+    const isDone = li.classList.contains(DONE);
+    this.saveTask(taskId, isDone);
+  }
+
   addToHtmlList(task) {
     const ul = document.querySelector("ul");
     const li = document.createElement("li");
@@ -32,11 +46,14 @@ export default class HtmlService {
     const button = document.createElement("button");
 
     li.setAttribute("data-item-id", task.id);
+    li.addEventListener("click", () => this.toggleTask(li));
+
     span.textContent = task.description;
+
     button.textContent = "X";
 
     if (task.done) {
-      li.classList.add('done');
+      li.classList.add(DONE);
     }
 
     li.appendChild(span);
