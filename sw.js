0f8c715487cf3b27ef0ce2d4deafd76ec5a0373b
AndreOneti <andreluiz.onete@hotmail.com>
2020-09-26 16:29:30 -0300

    Cache clean up

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
-const cacheName = 'app-shell-v2';
+const cacheName = 'app-shell-v3';
 const assetsToCache = [
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
   'https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
   'https://fonts.gstatic.com/s/roboto/v20/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
-  'https://fonts.googleapis.com/css?family=Roboto:400,700',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'assets/images/pwa-logo.png',
-  'assets/js/material.min.js',
-  'assets/css/style.css',
-  'assets/js/app.js',
-  'favicon.ico',
-  'index.html',
-  '/'
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://unpkg.com/dexie@3.0.2/dist/dexie.min.js',
+  './assets/js/material.min.js',
+  './assets/js/TodoService.js',
+  './assets/js/HtmlService.js',
+  './assets/css/style.css',
+  './assets/js/app.js',
+  './favicon.ico',
+  './index.html',
+  './'
 ];
 
 function removeOldCache(key) {
@@ -30,18 +32,6 @@ async function cacheStaticAssets() {
   const cache = await caches.open(cacheName);
   return cache.addAll(assetsToCache);
 }
-
-self.addEventListener('install', event => {
-  console.log('[Service Worker] Installing Service Worker...');
-  event.waitUntil(cacheStaticAssets());
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('[Service Worker] Activating Service Worker...');
-  event.waitUntil(cacheCleanup());
-  return self.clients.claim();
-});
 
 async function networkFirst(request) {
   try {
@@ -62,6 +52,18 @@ async function cacheFirst(request) {
   }
 }
 
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing Service Worker...');
+  event.waitUntil(cacheStaticAssets());
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activating Service Worker...');
+  event.waitUntil(cacheCleanup());
+  return self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(networkFirst(event.request));
+  event.respondWith(cacheFirst(event.request));
 });
